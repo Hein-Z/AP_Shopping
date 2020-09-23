8ffd8a5a0ed9990c3b342179726cf32f5212a545
Hein-Z <ingyin29122000@gamil.com>
2020-09-23 16:36:11 +0630

    Product Management:: Add Product

--- shop/admin/ProductsManagement.php
+++ shop/admin/ProductsManagement.php
@@ -3,7 +3,8 @@
 
 class ProductsManagement
 {
-    public function show($data,$pdo){
+    public function show($data, $pdo)
+    {
 
 
         if (!empty($_GET['page_no'])) {
@@ -42,7 +43,59 @@ class ProductsManagement
         }
 
 
-        return [$total_page, $products,$page_no,$offset];
+        return [$total_page, $products, $page_no, $offset];
     }
+
+    public function add($data, $file,$pdo)
+    {
+
+        if (!empty($data)) {
+
+            $targetFile = 'product_img/' . rand(time(), time()) . $file['image']['name'];
+            $imageName = rand(time(), time()) . $file['image']['name'];
+            $imageType = pathinfo($targetFile, PATHINFO_EXTENSION);
+
+            if (empty($file['image']['name']) || empty($data['name']) || empty($data['description']) ||
+                empty($data['price']) || empty($data['quantity']) || empty($data['category_id'])) {
+
+                if ($imageType != 'png' && $imageType != 'jpg' && $imageType != 'jpeg')
+                    $error['image'] = 'Image must be jpg, png or jpeg';
+                if(empty($data['name']))
+                    $error['name']='name cannot be blank';
+                if(empty($data['description']))
+                    $error['description']='description cannot be blank';
+                if(empty($data['price']))
+                    $error['price']='price cannot be blank';
+                if(empty($data['quantity']))
+                    $error['quantity']='quantity cannot be blank';
+                if(empty($data['category_id']))
+                    $error['category']='category cannot be blank';
+                if(empty($file['image']['name']))
+                    $error['image']='image cannot be blank';
+
+                return $error;
+
+            } else {
+                move_uploaded_file($file['image']['tmp_name'], $targetFile);
+
+                $sql = "INSERT INTO products(name,description,image,price,quantity,category_id) VALUES(:name,:description,:image,:price,:quantity,:category_id)";
+                $pdo_statement = $pdo->prepare($sql);
+                $result = $pdo_statement->execute(
+                    array(':name' => $data['name'], ':description' => $data['description'], ':image' => $imageName, ':price' => $data['price'],
+                        ':quantity' => $data['quantity'], ':category_id' => $data['category_id']));
+
+                if (isset($result)) {
+                    echo "<script>alert('record is added');window.location.href='index.php';</script>";
+                } else {
+                    echo "<script>alert('record cannot be added');</script>";
+                }
+            }
+
+        }
+
+
+    }
+
+
 }
 

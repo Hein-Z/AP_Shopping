e887891bc22bca57738e25f6ae49bad51575a032
Hein-Z <ingyin29122000@gamil.com>
2020-09-23 19:48:51 +0630

    show account name

--- shop/admin/product_edit.php
+++ shop/admin/product_edit.php
@@ -79,6 +79,8 @@ $categories = $stmt->fetchAll();
             </div>
 
             <div class="form-group">
+                <img src="product_img/<?php echo $product['image']; ?>" width="100" alt="">
+                <br>
                 <label>Product Image</label>
                 <input type="file" name='image' class="form-control" placeholder="Fill Product's Image">
                 <small class="text-danger"> * <?php echo isset($error['image']) ? $error['image'] : ''; ?> </small>

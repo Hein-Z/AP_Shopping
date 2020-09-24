d92008025ce50734b54cb320b11d779ca59294f5
Hein-Z <ingyin29122000@gmail.com>
2020-09-25 04:15:27 +0630

    index

--- shop/footer.php
+++ shop/footer.php
@@ -1,11 +1,10 @@
-</div>
-</div>
-</div>
 
+</div>
+</div>
 
 
 <!-- start footer Area -->
-<footer class="footer-area section_gap">
+<footer class="">
 <div class="container">
 <div class="footer-bottom d-flex justify-content-center align-items-center flex-wrap">
   <p class="footer-text m-0"><!-- Link back to Colorlib can't be removed. Template is licensed under CC BY 3.0. -->
